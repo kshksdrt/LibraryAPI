03ffd097c00e35504f3110b2d593ae487793b761
kshksdrt <kaushik.sdrt@gmail.com>
2022-11-30 22:18:59 +0100

    Migration to avoid recursion

--- Db/Models/Book.cs
+++ Db/Models/Book.cs
@@ -2,5 +2,4 @@ public class Book
 {
   public int BookId { get; set; }
   public string Name { get; set; }
-  public Author Author { get; set; }
 }
--- Migrations/20221130211230_RemoveAuthorDefinitionInBookToAvoidRecursion.Designer.cs
+++ Migrations/20221130211230_RemoveAuthorDefinitionInBookToAvoidRecursion.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221130211230_RemoveAuthorDefinitionInBookToAvoidRecursion")]
+    partial class RemoveAuthorDefinitionInBookToAvoidRecursion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
